d057bf9aafde0064c0cbd6df333b3e57f35414b1
theinfin1ty <anantgaurav1010@gmail.com>
2021-07-26 16:00:24 +0530

    Added APIs to add employee, view all employees and see employee by ID

--- app.js
+++ app.js
@@ -3,8 +3,53 @@ const fs = require('fs');
 const ExpressError = require('./utils/ExpressError');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("Home!");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const path = __dirname + '/data/employee.txt';
+
+app.get('/employee', (req, res) => {
+    if(fs.existsSync(path)) {
+        fs.readFile(path, 'utf8', function(err, data) {
+            var editedData = data.slice(0, -2);
+            const contents = JSON.parse("[" + editedData + "]");
+            res.send(contents);
+        });
+    }
+    else{
+        res.send("No Employee Record Found");
+    }
+});
+
+app.post('/employee', (req, res) => {
+    const content = req.body;
+    const employee = JSON.stringify(content);
+    fs.writeFile(path, employee + ',\n', { flag: 'a+' }, (err) => {
+        if(err) {
+            return res.send("An error occured while saving file").send(err);
+        }
+        res.send("Employee record saved!");
+    }); 
+});
+
+app.get('/employee/:id', (req, res) => {
+    const { id } = req.params;
+
+    if(fs.existsSync(path)) {
+        fs.readFile(path, 'utf8', function(err, data) {
+            var editedData = data.slice(0, -2);
+            const contents = JSON.parse("[" + editedData + "]");
+            for(var i=0; i < contents.length; i++) {
+                if(contents[i].ID == id) {
+                    return res.send(contents[i]);
+                }
+            }
+            res.send("Employee not found");
+        });
+    }
+    else{
+        res.send("No Employee Record Found");
+    }
 });
 
 app.all('*', (req, res, next) => {
